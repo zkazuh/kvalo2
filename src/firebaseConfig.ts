7d4bad0079fd05b7b8f034982e47fd55f27b0b72
Danilo Cercal Lara <danilocercal@proton.me>
2025-08-13 08:10:50 -0300

    Security improve

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: import.meta.env.apiKey,
+  authDomain: import.meta.env.authDomain,
+  projectId: import.meta.env.projectId,
+  storageBucket: import.meta.env.storageBucket,
+  messagingSenderId: import.meta.env.messagingSenderId,
+  appId: import.meta.env.appId
+};
